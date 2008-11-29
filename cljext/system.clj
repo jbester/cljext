01c3489c0190a29ea1d6d3861d43f774c96d96d8
Jeffrey Bester <jbester@gmail.com>
2008-11-29 16:17:54 -0500

    added system wrapping functions

---
+++ cljext/system.clj
@@ -0,0 +1,70 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; File     : system.clj
+;; Function : system library
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Send comments or questions to code at freshlime dot org
+;; $Id$
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Copyright (c) 2008, J. Bester
+;; All rights reserved.
+;;
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions are met:
+;;     * Redistributions of source code must retain the above copyright
+;;       notice, this list of conditions and the following disclaimer.
+;;     * Redistributions in binary form must reproduce the above copyright
+;;       notice, this list of conditions and the following disclaimer in the
+;;       documentation and/or other materials provided with the distribution.
+;;     * The name of the authors names of its contributors may be used to 
+;;       endorse or promote products derived from this software without
+;;       specific prior written permission.
+;;
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ``AS IS'' AND ANY
+;; EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+;; WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+;; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+;; DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+;; (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+;; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+;; ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+;; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(ns cljext.system
+  (:refer-clojure)
+  (:import [java.io BufferedReader InputStreamReader]))
+
+(defn system
+  "(system cmd)
+cmd - string to execute
+
+Returns:
+output of command as list of strings"
+  ([cmd]
+     ;; spawn process and capture output
+     (let [proc (.exec (Runtime/getRuntime) cmd)
+	   stdout (java.io.BufferedReader. (java.io.InputStreamReader. (.getInputStream proc)))]
+       ;; wait for completion
+       (.waitFor proc)
+       ;; collect output
+       (loop [result []]
+	 (let [line (.readLine stdout)]
+	   (if (nil? line)
+	     result
+	     (recur (conj result line))))))))
+       
+     
+(defn exit
+  ([status]
+     (System/exit status)))
+
+(defn getenv
+  ([]
+     (System/getenv))
+  ([str]
+     (System/getenv #^String str)))
+
+(defn gc
+  ([]
+     (System/gc)))
