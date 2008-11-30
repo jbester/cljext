a894bb9b172829e0b74919fce335001b983a43f7
Jeffrey Bester <jbester@gmail.com>
2008-11-30 00:17:15 -0500

    initial commit of formula macro

--- cljext/math.clj
+++ cljext/math.clj
@@ -546,5 +546,63 @@ GCD(A,B)==GCD(B,A%B)
 		      (if (even? n)
 			(* (+ (* 2 t1) t2) t2)
 			(+ (* t1 t1) (* t2 t2)))))))))
-		    
-		  
+	
+
+;;pemdas
+(def +precedence+
+     {'** 5,
+      '* 4,
+      '/ 3,
+      '+ 2,
+      '- 1})
+
+(def +highest-precedence+ 5)
+
+(defn- operator?
+  "Check if is valid operator"
+  ([sym]
+     (not (nil? (get +precedence+ sym)))))
+
+(defn find-lowest-precedence
+  "find the operator with lowest precedence; search from left to right"
+  ([seq]
+     ;; loop through terms in the sequence
+     (loop [idx 0
+	    seq seq
+	    lowest-idx nil
+	    lowest-prec +highest-precedence+]
+       ;; nothing left to process
+       (if (empty? seq)
+	 ;; return lowest found
+	 lowest-idx
+	 ;; otherwise check if current term is lower
+	 (let [prec (get +precedence+ (first seq))]
+	   ;; is of lower or equal precedence
+	   (if (and prec (<= prec lowest-prec))
+	     (recur (inc idx) (rest seq)
+		    idx prec)
+	     ;; is of high precedence therefore skip for now
+	     (recur (inc idx) (rest seq)
+		    lowest-idx lowest-prec)))))))
+
+(defn infix-to-prefix
+  "Convert from infix notation to prefix notation"
+  ([seq]
+     (cond 
+      ;; handle term only
+      (not (seq? seq)) seq
+      ;; handle sequence containing only one term
+      (= (count seq) 1) (first seq)
+      ;; handle all other cases
+      true (let [lowest (find-lowest-precedence seq)]
+	     (if (nil? lowest) ;; nothing to split
+	       seq
+	       ;; (a b c) bind a to hd, c to tl, and b to op
+	       (let [[hd tl] (split-at lowest seq)
+		     op (first tl)
+		     tl (rest tl)]
+		 (list op (infix-to-prefix hd) (infix-to-prefix tl))))))))
+
+(defmacro formula
+  ([& equation]
+     (infix-to-prefix equation)))
