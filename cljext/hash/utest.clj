307d47c42184e267d03ba80ae4a118d53b1c7125
Jeffrey Bester <jbester@gmail.com>
2008-12-07 12:40:54 -0500

    added hash unit test

---
+++ cljext/hash/utest.clj
@@ -0,0 +1,25 @@
+(ns cljext.hash.utest
+    (:refer-clojure)
+    (:require [cljext.hash :as hash]
+	      [clojure.contrib.test-is :as test-is]))
+
+(test-is/deftest test-hash-test
+		 (let [md5 (hash/create-hash 'MD5)]
+		   (test-is/is
+		    (= (hash/hash-to-string 
+			(do (md5 (.getBytes "Hello world"))
+			    (md5)))
+		       "3E25960A79DBC69B674CD4EC67A72C62")))
+		 (let [crc (hash/create-hash 'CRC32)]
+		   (test-is/is
+		    (= (hash/hash-to-string 
+			(do (crc (.getBytes "Hello world"))
+			    (crc)))
+		       "8BD69E52")))
+		 )
+
+
+(test-is/run-tests)
+
+
+ 
