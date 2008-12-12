ec1a413f95bca0e43079d982c54519620498aaae
Jeffrey Bester <jbester@gmail.com>
2008-12-11 22:09:41 -0500

    merged with experimental

--- cljext/math/utest.clj
+++ cljext/math/utest.clj
@@ -297,16 +297,16 @@
 
 (test-is/deftest test-summation
   (test-is/is 
-   (true? (= (math/summation i (range 10) i) (apply + (range 10)))))
+   (true? (= (math/summation [i (range 10)] i) (apply + (range 10)))))
   (test-is/is 
-   (true? (= (math/summation i (range 0 10 2) i) (apply + (range 0 10 2)))))
+   (true? (= (math/summation [i (range 0 10 2)] i) (apply + (range 0 10 2)))))
   )
 
 (test-is/deftest test-product
   (test-is/is 
-   (true? (= (math/product i (range 1 10) i) (apply * (range 1 10)))))
+   (true? (= (math/product [i (range 1 10)] i) (apply * (range 1 10)))))
   (test-is/is 
-   (true? (= (math/product i (range 1 10 2) i) (apply * (range 1 10 2)))))
+   (true? (= (math/product [i (range 1 10 2)] i) (apply * (range 1 10 2)))))
   )
 
 
