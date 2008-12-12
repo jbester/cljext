90f62a037a0ab31b3a4777747c7cbe2093ebc4bb
Jeffrey Bester <jbester@gmail.com>
2008-12-11 20:46:51 -0500

    added additional map test

--- cljext/seq/utest.clj
+++ cljext/seq/utest.clj
@@ -126,6 +126,11 @@
 		 (test-is/is
 		  (= (seq/vector-map inc (range 10))
 		     '[1 2 3 4 5 6 7 8 9 10]))
+
+		 (test-is/is
+		  (= (seq/vector-map vector '(a b c) (range 10))
+		     '[[a 0] [b 1] [c 2]]))
+
 		 )
 
 (test-is/deftest test-vector-filter
